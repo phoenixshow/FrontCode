3f7479c10dff3ce7d6f8cd3003495525f1a832df
phoenixshow <965550123@qq.com>
2018-09-21 11:53:36 +0800

    别克汇首页ajax登录、注册页表单验证

--- buickmall/js/register.js
+++ buickmall/js/register.js
@@ -1,5 +1,5 @@
 $(function(){
-	//决定最终是否可以提交表单
+	//定义变量，决定最终是否可以提交表单
 	var error_name = false;//默认没有错误
 	var error_pwd = false;
 	var error_cpwd = false;
@@ -7,43 +7,42 @@ $(function(){
 	var error_allow = false;//是否勾选了协议
 
 	//失去焦点时验证用户名
-	$('#username').blur(function(){
+	$('#username').blur(function() {
 		check_username();
-	})
-
+	});
 	//获取焦点时隐藏提示信息
-	$('#username').focus(function(){
+	$('#username').focus(function() {
 		$(this).prev().hide();
-	})
+	});
 
 	//密码
-	$('#password1').blur(function(){
+	$('#password1').blur(function() {
 		check_pwd();
-	})
-	$('#password1').focus(function(){
+	});
+	$('#password1').focus(function() {
 		$(this).prev().hide();
-	})
+	});
 
 	//确认密码
-	$('#password2').blur(function(){
+	$('#password2').blur(function() {
 		check_cpwd();
-	})
-	$('#password2').focus(function(){
+	});
+	$('#password2').focus(function() {
 		$(this).prev().hide();
-	})
+	});
 
 	//邮箱
-	$('#email').blur(function(){
+	$('#email').blur(function() {
 		check_email();
-	})
-	$('#email').focus(function(){
+	});
+	$('#email').focus(function() {
 		$(this).prev().hide();
-	})
+	});
 
 	//协议
 	$('#allow').click(function() {
 		//如果复选框的属性是已勾选
-		if($(this).prop('checked') == true){
+		if($(this).prop('checked')==true){
 			error_allow = false;
 			$('.error_tip2').hide();
 		}else{
@@ -54,7 +53,7 @@ $(function(){
 
 	function check_username(){
 		var val = $('#username').val();
-		var re = /^\w{5,15}$/;//匹配数字字母下划线，5-15位
+		var re = /^\w{5,15}$/i;//匹配字母数字下划线，5到15位，忽略大小写
 
 		if(val == ''){
 			$('#username').prev().html('用户名不能为空！');
@@ -63,7 +62,7 @@ $(function(){
 			return;
 		}
 
-		if(re.test(val)){//匹配上了就是正确的
+		if(re.test(val)){
 			error_name = false;
 		}else{
 			$('#username').prev().html('用户名是包含数字、字母、下划线的5-15位字符');
@@ -75,7 +74,7 @@ $(function(){
 
 	function check_pwd(){
 		var val = $('#password1').val();
-		var re = /^[a-zA-Z0-9\#\&\$]{6,16}$/;//[]表示范围，匹配数字字母特殊符号，6-16位
+		var re = /^[a-zA-Z0-9@\$\*\.\!\?]{6,16}$/;//[]表示范围，允许字母数字@$*.!?，6-16位
 
 		if(val == ''){
 			$('#password1').prev().html('密码不能为空！');
@@ -84,10 +83,10 @@ $(function(){
 			return;
 		}
 
-		if(re.test(val)){//匹配上了就是正确的
+		if(re.test(val)){
 			error_pwd = false;
 		}else{
-			$('#password1').prev().html('密码是包含数字、字母、#&$的6-16位字符');
+			$('#password1').prev().html('密码是包含数字、字母、@$*.!?的6-16位字符');
 			$('#password1').prev().show();
 			error_pwd = true;
 			return;
@@ -98,10 +97,10 @@ $(function(){
 		var val = $('#password1').val();
 		var cval = $('#password2').val();
 
-		if(val == cval){//如果一致是正确的
+		if(val == cval){
 			error_cpwd = false;
 		}else{
-			$('#password2').prev().html('两次输入的密码不一致！');
+			$('#password2').prev().html('再次输入的密码不一致！');
 			$('#password2').prev().show();
 			error_cpwd = true;
 			return;
@@ -110,9 +109,7 @@ $(function(){
 
 	function check_email(){
 		var val = $('#email').val();
-		//a@bc.com.cn
-		//a_b.c@bcd.com.net
-		var re = /^[a-zA-Z0-9][\w\.]*@[\w]+(\.[\w]{2,3}){1,2}$/;//字母或数字开头
+		var re = /^[a-zA-Z0-9][\w\.]*@[\w]+(\.[\w]{2,3}){1,2}$/;//字母或数字开头、一个或多个字母数字下划线、@、字母数字下划线、.字母数字下划线2-3位
 
 		if(val == ''){
 			$('#email').prev().html('邮箱不能为空！');
@@ -121,10 +118,10 @@ $(function(){
 			return;
 		}
 
-		if(re.test(val)){//匹配上了就是正确的
+		if(re.test(val)){
 			error_email = false;
 		}else{
-			$('#email').prev().html('邮箱格式不正确！');
+			$('#email').prev().html('邮箱格式不正确');
 			$('#email').prev().show();
 			error_email = true;
 			return;
@@ -138,8 +135,8 @@ $(function(){
 		check_cpwd();
 		check_email();
 
-		if(error_name || error_pwd || error_cpwd || error_email){
-			return false;//不能提交
+		if(!(error_name == false && error_pwd == false && error_cpwd == false && error_email == false && error_allow == false)){
+			return false;
 		}
 	});
 })
