9eaaa781ae4b182f6e9aa4fed55a54c74d24310e
phoenixshow <965550123@qq.com>
2018-09-21 12:16:21 +0800

    SQL注入

---
+++ SQL注入.sql
@@ -0,0 +1,17 @@
+select * from students where 
+userName='Tom' and password='123456';
+
+
+a' or password=
+ or '1'='1
+
+select * from students where 
+userName='a' or password=' and password=' or '1'='1';
+
+
+2018-09-21
+
+20180921
+2018921
+18921
+2018/09/21
